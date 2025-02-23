982fc7810e790c18894b759a51989e1bb5a5ff2f
Muhammad-Junaid-586 <154722812+Muhammad-Junaid-586@users.noreply.github.com>
2025-02-23 23:33:15 +0500

    remove the login functionality from app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -48,49 +48,16 @@ const App = () => {
         <Route path="/register" element={<Register />} />
         <Route
           path="/upload"
-          element={
-            localStorage.getItem("token") ? (
-              <Upload onDataFetched={handleDataFetched} />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
+          element={<Upload onDataFetched={handleDataFetched} />}
         />
         <Route path="/singledesign" element={<SingleDesigns />} />
         <Route path="/twodesign" element={<TwoDesigns />} />
         {/* <Route path="/kb" element={<DMCComponent students={students} />} /> */}
         <Route path="/it" element={<Dmc students={students} />} />
-        <Route
-          path="/dmcIntro"
-          element={
-            localStorage.getItem("token") ? (
-              <DmcIntro students={students} />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
-        <Route
-          path="/dmc"
-          element={
-            localStorage.getItem("token") ? (
-              <NewDmc students={students} />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
+        <Route path="/dmcIntro" element={<DmcIntro students={students} />} />
+        <Route path="/dmc" element={<NewDmc students={students} />} />
 
-        <Route
-          path="/newdmc"
-          element={
-            localStorage.getItem("token") ? (
-              <NewDmcLayout students={students} />
-            ) : (
-              <Navigate to="/login" />
-            )
-          }
-        />
+        <Route path="/newdmc" element={<NewDmcLayout students={students} />} />
         <Route path="*" element={<ComingSoon />} />
       </Routes>
       <Footer />
